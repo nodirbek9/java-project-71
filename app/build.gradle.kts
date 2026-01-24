f4647015afdad3dcfa33e5159390a94baff0003a
user <user@MacBook-Air-user.local>
2026-01-24 12:38:35 +0300

    first commit for project

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
