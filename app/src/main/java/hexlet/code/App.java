3d690288631603407b3291093d131be1aa0370fd
user <user@MacBook-Air-user.local>
2026-02-01 22:42:05 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Callable;
         version = "1.0",
         description = "Compares two configuration files and shows a difference."
 )
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 
     @Option(
             names = {"-f", "--format"},
